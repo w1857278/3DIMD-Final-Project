5533df409197a40375d40c18cf2ef1fb38b5fef7
langoug <148543303+langoug@users.noreply.github.com>
2023-11-06 03:42:39 +0000

    mouseinput

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Vector3 sc;
+    private Vector3 offsey;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+   public void OnMouseDown()
+    {
+        sc = Camera.main.WorldToScreenPoint(transform.position);
+        offsey = sc - Input.mousePosition;
+    }
+   public  void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + offsey);
+    }
+}
+
